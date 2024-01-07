e09aefe90255cee8ba622ac5626c65ca692022cc
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-07 11:30:54 +0900

    Update UserRepository.java

--- commerce/src/main/java/com/springboot/commerce/data/repository/UserRepository.java
+++ commerce/src/main/java/com/springboot/commerce/data/repository/UserRepository.java
@@ -4,7 +4,7 @@ package com.springboot.commerce.data.repository;
 import com.springboot.commerce.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// 예제 13.7
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User getByUid(String uid);
