e4095ad573b70e6d36e4978d3b4d77c616bcfb9d
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-09 21:04:38 +0900

    Update ProductRepository.java

--- commerce/src/main/java/com/springboot/commerce/data/repository/ProductRepository.java
+++ commerce/src/main/java/com/springboot/commerce/data/repository/ProductRepository.java
@@ -9,14 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    /*
-    아래 메소드는 책에서 소개하고 있는 Repository의 메소드 명명 규칙 예시입니다.
-
-    StartingWith(=StartWith) , EndingWith(=EndWith),
-    Containing(=Contains), Like
-    SQL 쿼리문에서 값의 일부를 포함하는 값을 추출할 때 사용하는 '%' 키워드와 동일한 역할
-
-    */
+ 
     List<Product> findByNameLike(String name);
 
     List<Product> findByNameContaining(String name);
@@ -26,10 +19,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByNameStartsWith(String name);
 
     List<Product> findByNameEndsWith(String name);
-
-    /*
-
-     */
+   
 
     List<Product> findByPriceGreaterThan(int price);
 
