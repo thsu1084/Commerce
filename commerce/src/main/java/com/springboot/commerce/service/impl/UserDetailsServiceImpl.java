15ed0ed539c82831025a44221c2be710695df942
thsu1084 <98458216+thsu1084@users.noreply.github.com>
2024-01-10 13:17:11 +0900

    Update UserDetailsServiceImpl.java

--- commerce/src/main/java/com/springboot/commerce/service/impl/UserDetailsServiceImpl.java
+++ commerce/src/main/java/com/springboot/commerce/service/impl/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-// 예제 13.8
+
 @RequiredArgsConstructor
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
